f5437d844d59b0efde1c2d2ca852e0959489928d
Joao Ferreira <joaojalf@gmail.com>
2024-12-17 15:50:59 +0100

    ignore eslint during builds

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
   sassOptions: {
     silenceDeprecations: ['legacy-js-api'],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default withNextIntl(nextConfig);
