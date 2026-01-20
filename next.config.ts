1d23807d2eed00d4b7f06ee55a018812547ed860
Joao Ferreira <joaojalf@gmail.com>
2026-01-20 17:01:25 +0100

    chore: update eslint to version 9.39.2 and remove eslint config from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,11 +7,6 @@ const nextConfig: NextConfig = {
   sassOptions: {
     silenceDeprecations: ['legacy-js-api'],
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default withNextIntl(nextConfig);
